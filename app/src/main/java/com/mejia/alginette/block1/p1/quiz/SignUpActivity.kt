51c4cf9e3025f12a22ec973dbf1a0d2c79e94e66
Alginette <lgme.mejia.up@phinmaed.com>
2023-12-08 23:51:14 -0800

    .

---
+++ app/src/main/java/com/mejia/alginette/block1/p1/quiz/SignUpActivity.kt
@@ -0,0 +1,12 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mejia.alginette.block1.p1.quiz.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+}
